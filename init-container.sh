289e144fd1b66e7a38365951d355c9890248d9d6
Ameet Konnur <ameetk@live.in>
2019-03-29 16:40:27 +0530

    20190330

--- init-container.sh
+++ init-container.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+crontab -l > mycron
+echo "00 02 * * * python /my-app-code/billing-app/get-usage.py && python /my-app-code/billing-app/get-azure-subscription-data.py && python /my-app-code/billing-app/sendmail.py" >> mycron
+crontab mycron
+rm mycron
 service ssh start
 /usr/sbin/sshd
 superset init
 superset runserver -d
+
 
